d981e88bd7740c9e9c3fee323342522f4326c0c9
Govind2003 <89623058+Govind2003@users.noreply.github.com>
2021-10-27 21:27:26 -0500

    Changes in endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+let x=0
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  x++
+  res.send("“User accesses are: "+x);
 });
 
 module.exports = router;
